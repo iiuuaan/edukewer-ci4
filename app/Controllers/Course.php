6a41f408370b0ccd44f471eb53b0fc6d55c82f81
BudiSetiawan <50059750+1BudiSetiawan1@users.noreply.github.com>
2025-05-30 06:50:29 +0800

    add order by in course controllers

--- app/Controllers/Course.php
+++ app/Controllers/Course.php
@@ -2,17 +2,8 @@
 
 namespace App\Controllers;
 
-use App\Models\CourseModel;
-
 class Course extends BaseController
 {
-    public function index()
-    {
-        $model = new CourseModel();
-        $course = $model->find(1); // assuming you're fetching course with ID = 1
-
-        return view('user/course', ['course' => $course]);
-    }
 
     public function view($id)
     {
@@ -24,7 +15,10 @@ class Course extends BaseController
             throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound("Course not found");
         }
 
-        $data['modules'] = $moduleModel->where('course_id', $id)->findAll();
+        $data['modules'] = $moduleModel
+            ->where('course_id', $id)
+            ->orderBy('module_number', 'ASC')
+            ->findAll();
 
         return view('user/course', $data);
     }
